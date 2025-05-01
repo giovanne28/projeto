dd1bf41ff18a55fea9333628ded55c0d6202e675
giovanne28 <giovanneh.araujo05@gmail.com>
2025-05-01 17:23:55 -0300

    estados

---
+++ whi.py
@@ -0,0 +1,41 @@
+print ('Este programa mostra os estados de cada região')
+
+
+regiao = input('Região:')
+if regiao == 'Nordeste':
+    print ('''Os estados da região nordestes são:
+    - Alagoas
+    - Bahia
+    - Ceará
+    - Maranhão
+    - Paraíba
+    - Pernambuco
+    - Piauí
+    - Rio Grande do Norte
+    - Sergipe''')
+if regiao == 'Sul':
+    print ('''Os estados da região sul são:
+    - Paraná
+    - Rio Grande do Sul
+    - Santa Catarina''')
+if regiao == 'Sudeste':
+    print ('''Os estados da região sudeste são:
+    - Espírito Santo
+    - Minas Gerais
+    - Rio de Janeiro
+    - São Paulo''')
+f regiao == 'Norte':
+    print ('''Os estados da região norte são:
+    - Acre
+    - Amapá
+    - Amazonas
+    - Pará
+    - Rondônia
+    - Roraima
+    - Tocantins''')
+if regiao == 'Centro-Oeste':
+    print ('''Os estado da região centro-oeste são:
+    - Distrito Federal
+    - Goiás
+    - Mato Grosso
+    - Mato Grosso do Sul''')
