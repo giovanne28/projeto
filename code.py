52616f4a4cca448081868c0121cc2136787395fa
giovanne28 <giovanneh.araujo05@gmail.com>
2025-05-31 13:51:51 -0300

    adicionando nova pasta

---
+++ code.py
@@ -0,0 +1,9 @@
+print('informe sua nota')
+n1= float(input('nota:'))
+n2= float(input('nota:'))
+n3= float(input('nota:'))
+media= (n1+n2+n3)/3
+if media >= 7:
+  print('esta na media')
+else:
+  print('reprovado')
