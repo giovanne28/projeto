5008a3cb628b49b2e886efbec0993f7564ce8cc0
giovanne28 <giovanneh.araujo05@gmail.com>
2025-08-28 21:24:17 -0300

    codando

---
+++ brawl.py
@@ -0,0 +1,31 @@
+#projeto meu
+brawlers= input('Digite um brawler para saber sua raridade:')
+if brawlers== 'Shelly':
+   print('Raridade: ⚪️Brawler inicial')
+if brawlers== 'Nita' or brawlers == 'Colt' or brawlers == 'Bull' or brawlers == 'Brock' or brawlers == 'El primo' or brawlers == 'Barley' or brawlers == 'Poco' or brawlers == 'Rosa':
+   print('Raridade: 🟢Raro')
+if brawlers== 'Jessie' or brawlers == 'Dynamike' or brawlers == 'Tick' or brawlers == '8-bit' or brawlers == 'Rico' or brawlers == 'Darryl' or brawlers == 'Penny' or brawlers == 'Carl' or brawlers == 'Jacky' or brawlers == 'Gus':
+   print('Raridade: 🔵Super raro')
+if brawlers== 'Bo' or brawlers == 'Emz' or brawlers == 'Stu' or brawlers == 'Piper' or brawlers == 'Pam' or brawlers == 'Frank' or brawlers == 'Bibi' or brawlers == 'Bea' or brawlers == 'Nani' or brawlers == 'Grow' or brawlers == 'Edgar' or brawlers == 'Bonnie' or brawlers == 'Griff' or brawlers == 'Gale' or brawlers == 'Colette' or brawlers == 'Belle' or brawlers == 'Ash' or brawlers == 'Lola' or brawlers == 'Sam' or brawlers == 'Mandy' or brawlers == 'Maisie' or brawlers == 'Hank' or brawlers == 'Pearl' or brawlers == 'Pérola' or brawlers == 'Larrie & Lawrie' or brawlers == 'Larrie e Lawrie' or brawlers == 'LeL' or brawlers == 'Angelo' or brawlers == 'Berry' or brawlers == 'Shade' or brawlers == 'Meeple' or brawlers == 'Trunk':
+   print('Raridade: 🟣Épico')
+if brawlers== 'Mortis' or brawlers == 'Tara' or brawlers == 'Eugênio' or brawlers == 'Max' or brawlers == 'Mister.P' or brawlers == 'Mister.p' or brawlers == 'Sprout' or brawlers == 'Byron' or brawlers == 'Squeak' or brawlers == 'Lou' or brawlers == 'Coronel Ruffs' or brawlers == 'Ruffs' or brawlers == 'Buzz' or brawlers == 'Fang' or brawlers == 'Eve' or brawlers == 'Janet' or brawlers == 'Otis' or brawlers == 'Buster' or brawlers == 'Gray' or brawlers == 'R-T' or brawlers == 'Willow' or brawlers == 'Doug' or brawlers == 'Chuck' or brawlers == 'Charlie' or brawlers == 'Mico' or brawlers == 'Melodie' or brawlers == 'Lily' or brawlers == 'Clancy' or brawlers == 'Moe' or brawlers == 'Juju' or brawlers == 'Ollie' or brawlers == 'Finx' or brawlers == 'Lumi' or brawlers == 'Jae-yong' or brawlers == 'Ali':
+   print('Raridade: 🔴Mítico')
+if brawlers== 'Spike' or brawlers == 'Corvo' or brawlers == 'Leon' or brawlers == 'Sandy' or brawlers == 'Amber' or brawlers == 'Meg' or brawlers == 'Wattson' or brawlers == 'Surge' or brawlers == 'Chester' or brawlers == 'Cordelius' or brawlers == 'Kit' or brawlers == 'Draco' or brawlers == 'Kenji':
+   print('Raridade: 🟡Lendário')
+if brawlers== 'Kaze':
+   print('Raridade: 🟠Ultralendário')
+classe= input('Digite um brawler para saber sua classe:')
+if classe== 'Shelly' or classe == 'Nita' or classe == 'Moe' or classe == '8-bit' or classe == 'Colt' or classe == 'Carl' or classe == 'Colette' or classe == 'Rico' or classe == 'Lola' or classe == 'Tara' or classe == 'Eve' or classe == 'R-T' or classe == 'Pearl' or classe == 'Pérola' or classe == 'Clancy' or classe == 'Spike' or classe == 'Chester' or classe == 'Wattson' or classe == 'Lumi':
+   print('Classe: Destruidor👊🏽')
+if classe== 'Barley' or classe == 'Dynamike' or classe == 'Tick' or classe == 'Juju' or classe == 'Grow' or classe == 'Sprout' or classe == 'Larrie e Lawrie':
+   print('Classe: Detonador🧨')
+if classe== 'Poco' or classe == 'Pam' or classe == 'Berry' or classe == 'Gus' or classe == 'Max' or classe == 'Byron' or classe == 'Coronel Ruffs' or classe == 'Ruffs' or classe == 'Doug' or classe == 'Gray' or classe == 'Kit' or classe == 'Jae-yong':
+   print('Classe: Suporte💊')
+if classe== 'Bull' or classe == 'El primo' or classe == 'Hank' or classe == 'Frank' or classe == 'Bibi' or classe == 'Rosa' or classe == 'Darryl' or classe == 'Jack' or classe == 'Ash' or classe == 'Buster' or classe == 'Meg' or classe == 'Ollie' or classe == 'Draco' or classe == 'Trunk':
+   print('Classe: Tanque🛡')
+if classe== 'Brock' or classe == 'Piper' or classe == 'Mandy' or classe == 'Janet' or classe == 'Bea' or classe == 'Angelo' or classe == 'Nani' or classe == 'Maisie' or classe == 'Bonnie' or classe == 'Belle':
+   print('Classe: Tiro preciso🎯')
+if classe== 'Jessie' or classe == 'Bo' or classe == 'Mister.p' or classe == 'Mister.P' or classe == 'Lou' or classe == 'Meeple' or classe == 'Gale' or classe == 'Griff' or classe == 'Penny' or classe == 'Emz' or classe == 'Eugênio' or classe == 'Squeak' or classe == 'Charlie' or classe == 'Otis' or classe == 'Willow' or classe == 'Chuck' or classe == 'Sandy' or classe == 'Amber' or classe == 'Finx':
+   print('Classe: Controle🕹')
+if classe== 'Mortis' or classe == 'Melodie' or classe == 'Edgar' or classe == 'Leon' or classe == 'Sam' or classe == 'Stu' or classe == 'Mico' or classe == 'Cordelius' or classe == 'Corvo' or classe == 'Kenji' or classe == 'Buzz' or classe == 'Lily' or classe == 'Shade' or classe == 'Fang' or classe == 'Ali' or classe == 'Kaze':
+   print('Classe: Algoz🗡')
